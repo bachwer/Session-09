3d875970f7285ff2d4c184c58e2de46a03bdea7f
bachwer <110678064+bachwer@users.noreply.github.com>
2024-11-29 17:29:21 +0700

    Update [Luyện tập]1. Thêm phần tử vào mảng.c

--- [Luyện tập]1. Thêm phần tử vào mảng.c
+++ [Luyện tập]1. Thêm phần tử vào mảng.c
@@ -1,42 +1,30 @@
 #include <stdio.h>
-
-int main () {
-    int array[100];
-    int vitri;
-    int number;
-    int Y;
-    
-    printf("Nhap so phan tu muon nhap:");
-    scanf("%d",&Y);
-   
-    
-    while (Y>0) {
-         printf("Nhap vi tri:");
-         scanf("%d", &vitri);
-         
-    if(vitri<0 || vitri>100) {
-        printf("Khong hop le");
-        return 1;
+int main(){
+    int n;
+    int num1[100];
+    printf("Nhap phan tu: ");
+    scanf("%d", &n);
+    num1[n];
+    for(int i = 0; i < n; i++){
+        printf("Nhap pt %d: ", i + 1);
+        scanf("%d", &num1[i]);
     }
+    int pos;
+    int valuef;
+    printf("nhap vi tri: ");
+    scanf("%d", &pos);
+    pos--;
     
-    for (int i=0 ; i <= 100; i++){
-        if (i == vitri){
-            printf("Nhap gia tri\n");
-            scanf("%d",&number);
-            array[i] = number;
-            break;
-        }else {
-            array[i] = 0;
-        }
-    } 
-        Y--;
+    num1[n + 1];
+    for(int i = n + 1; i >= pos; i--){
+        num1[i + 1] = num1[i];
     }
+    printf("Gia tri thay the: ");
+    scanf("%d", &valuef);
+    num1[pos] = valuef;
     
-    for (int i=0;i<=100;i++) {
-         printf("%d\n", array[i]);
-        
+    for(int i = 0; i < n + 1; i++){
+        printf("%d ", num1[i]);
     }
-   
-    
-    
+    return 0;
 }
